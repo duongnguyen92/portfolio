28716f05bbee25a95e70f0a1c8bedae399512b64
Duong Nguyen <duongnguyen.it92@gmail.com>
2015-07-16 18:31:47 +0700

    config portfolio
    Change config portfolio

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'dung_portfolio');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'nguyentuanduong');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'Duong12!@#');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
